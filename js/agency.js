c40fb53fbea638ec9aa327e83088767c8b26a87a
Pablo Delgado <pdelgadogp@gmail.com>
2020-03-27 21:31:18 +0100

    Smooth scroll

--- js/agency.js
+++ js/agency.js
@@ -19,16 +19,22 @@
       $(".logo-hidden").addClass("logo-display");
       $(".nav-link").addClass("nav-link-scroll");
       $(".line-navbar").addClass("nav-link-scroll");
+      $(".nav-item-hide").addClass("nav-item-show");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
       $(".logo-hidden").removeClass("logo-display");
       $(".nav-link").removeClass("nav-link-scroll");
       $(".line-navbar").removeClass("nav-link-scroll");
+      $(".nav-item-hide").removeClass("nav-item-show");
     }
   };
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+
+  var scrollToTop = function() {
+    $(window).scrollTop(0);
+  }
 
 })(jQuery); // End of use strict
