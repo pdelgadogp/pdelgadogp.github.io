8d170327c54897f05ab94b068d9a2300cb6eef33
Pablo Delgado <pdelgadogp@gmail.com>
2021-01-16 22:01:48 +0100

    Cambios

--- contacto.php
+++ contacto.php
@@ -1,13 +1,19 @@
 <?php 
-if(isset($_POST['submit'])){
+    echo "HOLA"
+    if(isset($_POST['submit'])){
+    
+    $js_code = 'console.log(' . json_encode("PRUEBA", JSON_HEX_TAG) . ');';
+    echo $js_code;
+
     $to = "pdelgadogp@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+    $name = $_POST['name'];
     $subject = "(Demihermana) NUEVO MENSAJE WEB";
-    $message = $first_name . " " . $last_name . " escribió lo siguiente:" . "\n\n" . $_POST['message'];
+    $message = $name . " escribió lo siguiente:" . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
+    $headers = "From: " . $from;
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";  
     mail($to,$subject,$message,$headers);
     // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
