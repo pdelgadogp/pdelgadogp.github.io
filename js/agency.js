8ac9c906f86f3aad86f7c9dfef86f196d8122695
Pablo Delgado <pdelgadogp@gmail.com>
2021-02-03 20:45:09 +0100

    Cambios

--- js/agency.js
+++ js/agency.js
@@ -22,14 +22,16 @@
         $(".nav-link").addClass("nav-link-scroll");
         $(".line-navbar").addClass("nav-link-scroll");
         $(".nav-item-hide").addClass("nav-item-show");
-        document.getElementById('myImage').src='img/logos/Isotipo-landing-web-2.png'
+        //document.getElementById('myImage').src='img/logos/Isotipo-landing-web-2.png'
+        document.getElementById('myImage').src='img/logos/Logo-Blanco-pequeñorosa.png'
       } else {
         $("#mainNav").removeClass("navbar-shrink");
         $(".logo-hidden").removeClass("logo-display");
         $(".nav-link").removeClass("nav-link-scroll");
         $(".line-navbar").removeClass("nav-link-scroll");
         $(".nav-item-hide").removeClass("nav-item-show");
-        document.getElementById('myImage').src='img/logos/Isotipo-landing-web-2blanco.png'
+        //document.getElementById('myImage').src='img/logos/Isotipo-landing-web-2blanco.png'
+        document.getElementById('myImage').src='img/logos/Logo-Blanco-pequeño.png'
       }
     } else {
       $(".nav-item-show").addClass("nav-item-hide");
@@ -43,7 +45,7 @@
 })(jQuery); // End of use strict
 
 
-var changeBackground = function(opt) {
+/*var changeBackground = function(opt) {
   var img = document.getElementById('SLIDE_BG');
   var style = img.currentStyle || window.getComputedStyle(img, false);
   var bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
@@ -72,7 +74,7 @@ var changeBackground = function(opt) {
 };
 
 // Will execute myCallback every 0.5 seconds 
-var intervalID = window.setInterval(changeBackground, 10000);
+var intervalID = window.setInterval(changeBackground, 10000);*/
 
 /*function myCallback() {
  // Your code here
